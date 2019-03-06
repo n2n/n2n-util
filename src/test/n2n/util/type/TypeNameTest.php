6860a0a97a295cfcfecd82ab5215c90bf63703bf
Andreas von Burg <a@von-burg.net>
2019-03-06 18:45:32 +0100

    type convert tests added

--- src/test/n2n/util/type/TypeNameTest.php
+++ src/test/n2n/util/type/TypeNameTest.php
@@ -14,4 +14,33 @@ class TypeNameTest extends TestCase {
 		
 		$this->assertTrue(TypeName::isValueA(true, 'scalar'));
 	}
+	
+	function testIsConvertable() {
+		$this->assertTrue('1' === TypeName::convertValue(1, 'string'));
+		$this->assertTrue('-1' === TypeName::convertValue(-1, 'string'));
+		$this->assertTrue('1.1' === TypeName::convertValue(1.1, 'string'));
+		$this->assertTrue('1' === TypeName::convertValue(true, 'string'));
+		$this->assertTrue('' === TypeName::convertValue(false, 'string'));
+		try {
+			TypeName::convertValue(null, 'string');
+			$this->fail('no ex thrown');
+		} catch (\InvalidArgumentException $e) {
+			$this->assertTrue(true);
+		}
+			
+		$this->assertTrue(1 === TypeName::convertValue('1', 'int'));
+		$this->assertTrue(-1 === TypeName::convertValue('-1', 'int'));
+		$this->assertTrue(1 === TypeName::convertValue(1, 'int'));
+		try {
+			TypeName::convertValue(null, 'string');
+			$this->fail('no ex thrown');
+		} catch (\InvalidArgumentException $e) {
+			$this->assertTrue(true);
+		}
+		try {
+			$this->assertTrue('1.1' === TypeName::convertValue(1.1, 'int'));
+		} catch (\InvalidArgumentException $e) {
+			$this->assertTrue(true);
+		}
+	}
 }
