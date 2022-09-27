7db8bf1d8d0411bdc5b6398feee910179117e0c2
Andreas von Burg <a@von-burg.net>
2022-09-27 16:10:56 +0200

    test added

---
+++ src/test/n2n/util/magic/SimpleMagicContextTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace n2n\util\magic;
+
+use PHPUnit\Framework\TestCase;
+
+class SimpleMagicContextTest extends TestCase {
+
+	function testCreate() {
+		$simpleMagicContext = new SimpleMagicContext();
+	}
+
+}
