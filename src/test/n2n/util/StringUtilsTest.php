abc1e10405946191ae4b95d72c88d5b0ed5db400
Thomas Guenther <guenther@hnm.ch>
2022-11-28 18:28:17 +0100

    Updated StringUtils::pretty

--- src/test/n2n/util/StringUtilsTest.php
+++ src/test/n2n/util/StringUtilsTest.php
@@ -13,4 +13,17 @@ class StringUtilsTest extends \PHPUnit\Framework\TestCase {
 		$dirtyString = '  ​äüö‍‍‍àéè+‌"*ç%‎‏&/	()=?€å≈асдфCuraçao£ ';
 		$this->assertEquals($cleanStr, StringUtils::clean($dirtyString, true));
 	}
+	
+	public function testPretty() {
+ 		$uglyString = 'Für einen Test';
+ 		$this->assertEquals($uglyString, StringUtils::pretty($uglyString));
+		
+ 		$this->assertEquals('Was Ist Denn Das', StringUtils::pretty('wasIstDennDas'));
+		
+		$this->assertEquals('Was DIst Denn Das', StringUtils::pretty('wasDIstDennDas'));
+		
+		$this->assertEquals('Super Duper', StringUtils::pretty('superDuper'));
+		$this->assertEquals('Super Duper Huper', StringUtils::pretty('super_duperHuper'));
+		$this->assertEquals('Super 234 Dingsel DUPER Holeradio', StringUtils::pretty('super234DingselDUPER_Holeradio'));
+	}
 }
