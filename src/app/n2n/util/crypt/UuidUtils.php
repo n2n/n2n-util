bb6a61139ff96128cdba760a77e32f7e22078d18
Andreas von Burg <a@von-burg.net>
2023-07-25 17:43:36 +0200

    Uuid

--- src/app/n2n/util/crypt/UuidUtils.php
+++ src/app/n2n/util/crypt/UuidUtils.php
@@ -12,7 +12,7 @@ class UuidUtils {
 	 * @return string
 	 * @throws \Exception
 	 */
-	function uuidv4(string $data = null): string {
+	static function uuidv4(string $data = null): string {
 		// Generate 16 bytes (128 bits) of random data or use the data passed into the function.
 		$data = $data ?? random_bytes(16);
 
