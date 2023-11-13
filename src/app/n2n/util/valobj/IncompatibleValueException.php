d304bece0de10315672e1016ec315d42414e7358
Andreas von Burg <a@von-burg.net>
2023-11-13 12:56:32 +0100

    exception helper added

--- src/app/n2n/util/valobj/IncompatibleValueException.php
+++ src/app/n2n/util/valobj/IncompatibleValueException.php
@@ -2,6 +2,16 @@
 
 namespace n2n\util\valobj;
 
+use n2n\util\ex\IllegalStateException;
+
 class IncompatibleValueException extends \Exception {
 
+	/**
+	 * @throws IncompatibleValueException
+	 */
+	public static function assertTrue($arg, string $exMessage = null): void {
+		if ($arg === true) return;
+
+		throw new IncompatibleValueException($exMessage ?? '');
+	}
 }
