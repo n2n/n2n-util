325a27a2dca6c105fdcdb0f89aa5bbd1a9c0efcf
andreas <a@von-burg.net>
2024-06-04 19:52:37 +0200

    MagicMethodInvoker namespace

--- src/app/n2n/util/magic/impl/MagicMethodInvoker.php
+++ src/app/n2n/util/magic/impl/MagicMethodInvoker.php
@@ -19,7 +19,7 @@
  * Bert Hofmänner.......: Idea, Frontend UI, Community Leader, Marketing
  * Thomas Günther.......: Developer, Hangar
  */
-namespace n2n\reflection\magic;
+namespace n2n\util\magic\impl;
 
 use n2n\core\N2N;
 use n2n\core\module\Module;
