1daef5ff2781e86ca18b92b9f991b0c6c25c1f13
Andreas von Burg <a@von-burg.net>
2024-09-04 20:03:17 +0200

    DateInterval

--- src/app/n2n/util/DateUtils.php
+++ src/app/n2n/util/DateUtils.php
@@ -206,7 +206,7 @@ class DateUtils {
 	}
 
 	static function dateInterval(int $y = 0, int $m = 0, int $d = 0, int $h = 0, int $i = 0, int $s = 0, float $f = 0): \DateInterval {
-		$dateInterval = new \DateInterval('');
+		$dateInterval = new \DateInterval('POY');
 		$dateInterval->y = $y;
 		$dateInterval->m = $m;
 		$dateInterval->d = $d;
