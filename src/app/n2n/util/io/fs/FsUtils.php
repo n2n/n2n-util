8dc862c0d6b5fc81e0ce3dc7da1783f0c5961ceb
nikolai <nikolai@schmid.guru>
2026-01-30 16:57:09 +0700

    FsUtils::prettyFileSize() roundUpDecimals

--- src/app/n2n/util/io/fs/FsUtils.php
+++ src/app/n2n/util/io/fs/FsUtils.php
@@ -8,19 +8,22 @@ class FsUtils {
 	const BYTES_PER_MB = 1024 * 1024;
 	const BYTES_PER_GB = 1024 * 1024 * 1024;
 
-	static function prettyFileSize(int $bytesNum): string {
-		if ($bytesNum >= self::BYTES_PER_GB) {
-			return round($bytesNum / self::BYTES_PER_GB, 2) . ' GB';
+	static function prettyFileSize(int $bytes, bool $roundUpDecimals = false): string {
+		$units = [
+				'GB' => self::BYTES_PER_GB,
+				'MB' => self::BYTES_PER_MB,
+				'KB' => self::BYTES_PER_KB
+		];
+
+		foreach ($units as $unit => $bytesPerUnit) {
+			if ($bytes >= $bytesPerUnit) {
+				$value = $bytes / $bytesPerUnit;
+				$value = $roundUpDecimals ? ceil($value * 100) / 100 : round($value, 2);
+
+				return $value . ' ' . $unit;
+			}
 		}
 
-		if ($bytesNum >= self::BYTES_PER_MB) {
-			return round($bytesNum / self::BYTES_PER_MB, 2) . ' MB';
-		}
-
-		if ($bytesNum >= self::BYTES_PER_KB) {
-			return round($bytesNum / self::BYTES_PER_KB, 2) . ' KB';
-		}
-
-		return $bytesNum . ' B';
+		return $bytes . ' B';
 	}
 }
