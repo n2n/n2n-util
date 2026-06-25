88cf52be142a01dd60514f91c5c71d578df4c52b
andreas <a@von-burg.net>
2026-06-25 17:55:47 +0200

    n2n\util\crypt moved to separate package

--- src/app/n2n/util/crypt/ex/RandomPseudoBytesException.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace n2n\util\crypt\ex;
-
-class RandomPseudoBytesException extends CryptRuntimeException {
-}
--- src/app/n2n/util/crypt/symmetric/SymmetricAlgorithm.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace n2n\util\crypt\symmetric;
-
-enum SymmetricAlgorithm: string {
-	case AES_128_GCM = 'aes-128-gcm';
-	case AES_192_GCM = 'aes-192-gcm';
-	case AES_256_GCM = 'aes-256-gcm';
-}
